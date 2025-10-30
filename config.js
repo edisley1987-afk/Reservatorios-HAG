70ee19b67d75030d5968b2e9b9473e44f5b2c027
edisley1987-afk <edisley1987@gmail.com>
2025-10-30 07:41:19 -0300

    config.js

--- config.js
+++ config.js
@@ -1,28 +1,32 @@
-// Configuration: user, pass and sensors
+// Configurações de usuário e sensores
 exports.USER = "hag";
-exports.PASS = "38195156";
+exports.PASS = "3819156";
 exports.SENSORS = {
   "Reservatorio_Elevador_current": {
     nome: "Reservatório Elevador",
-    leituraVazio: 4168,
+    leituraVazio: 0.004168,
+    leituraCheio: 0.007855,
     alturaRes: 1.45,
     capacidadeTotal: 20000
   },
   "Reservatorio_Osmose_current": {
     nome: "Reservatório Osmose",
-    leituraVazio: 505,
+    leituraVazio: 0.00505,
+    leituraCheio: 0.006492,
     alturaRes: 1.0,
     capacidadeTotal: 200
   },
   "Reservatorio_CME_current": {
     nome: "Reservatório CME",
-    leituraVazio: 4088,
+    leituraVazio: 0.004088,
+    leituraCheio: 0.004408,
     alturaRes: 0.45,
     capacidadeTotal: 1000
   },
   "Agua_Abrandada_current": {
     nome: "Reservatório Água Abrandada",
-    leituraVazio: 4008,
+    leituraVazio: 0.004008,
+    leituraCheio: 0.004929,
     alturaRes: 0.6,
     capacidadeTotal: 9000
   }
