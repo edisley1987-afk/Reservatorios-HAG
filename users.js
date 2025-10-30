19fd7caf785790cd72a862cf76744b4c511d9178
edisley1987-afk <edisley1987@gmail.com>
2025-10-30 13:37:41 -0300

    users.js

--- users.js
+++ users.js
@@ -1,4 +1,7 @@
-module.exports = [
-    { username: "hag", password: "1234" },
-    { username: "admin", password: "admin" }
+// users.js
+const users = [
+  { username: "hag", password: "1234" },
+  { username: "admin", password: "38195156" },
 ];
+
+module.exports = { users };
